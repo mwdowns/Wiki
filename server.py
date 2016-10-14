cb27431127021364145f1b37165dba50cffbbade
Matthew Downs <mwdowns@gmail.com>
2016-10-14 16:21:13 -0400

    adds code to wiki pages

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-import pg
+from flask import Flask, render_template, request, redirect, Markup
+from wiki_linkify import wiki_linkify
+import pg, datetime, markdown
 app = Flask("wiki")
 
 db = pg.DB(dbname="wiki")
@@ -19,29 +20,81 @@ def place_holder(page_name):
             query=query
         )
     else:
+        page_content = query[0].page_content
+        wiki_linkified_content = wiki_linkify(page_content)
+        print page_content
         return render_template(
             "placeholderpage.html",
             page_name = page_name,
-            query=query[0]
+            query=query[0],
+            page_content = Markup(markdown.markdown(wiki_linkified_content))
         )
 
 @app.route("/<page_name>/edit")
 def edit_page(page_name):
-    return render_template(
-        "edit.html",
-        page_name=page_name
-    )
+    query = db.query("select * from page where title = '%s'" % page_name).namedresult()
+    if len(query) == 0:
+        return render_template(
+            "edit.html",
+            page_name=page_name,
+            query=query
+        )
+    else:
+        return render_template(
+            "edit.html",
+            page_name=page_name,
+            query=query[0]
+        )
 
 @app.route("/<page_name>/save", methods=["POST"])
 def save_content(page_name):
-    print page_name
+    id = request.form.get("id")
     page_content = request.form.get("page_content")
-    db.insert (
-        "page",
-        page_content=page_content,
-        title=page_name
-    )
+    author_name = request.form.get("author_name")
+    last_mod_date = request.form.get("last_mod_date")
+    action = request.form.get("submit_button")
+    if action == "update":
+        db.update(
+            "page", {
+                "id": id,
+                "page_content": page_content,
+                "author_name": author_name,
+                "last_mod_date": last_mod_date,
+            }
+        )
+    elif action == "create":
+        db.insert (
+            "page",
+            title=page_name,
+            page_content=page_content,
+            author_name=author_name
+        )
+    else:
+        pass
     return redirect("/%s" % page_name)
+
+@app.route("/AllPages")
+def all_pages():
+    query = db.query("select title from page order by title;").namedresult()
+    return render_template(
+        "allpages.html",
+        query=query
+    )
+
+@app.route("/search")
+def search_pages():
+    search = request.args.get("search")
+    page = db.query("select title from page where title = '%s'" % search).namedresult()
+    print search
+    print page
+    if len(page) == 0:
+        return redirect("/%s" % search)
+    else:
+        return render_template(
+            "search.html",
+            search=search,
+            page=page
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
